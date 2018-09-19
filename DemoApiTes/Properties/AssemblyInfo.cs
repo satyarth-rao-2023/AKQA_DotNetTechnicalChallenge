2dd5c6acc64887a0f32f27ffe5632f1efe620767
TechTalkSaty <noreply@example.com>
2018-09-19 14:08:54 +0530

    Initial Commit

---
+++ DemoApiTes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DemoApiTes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DemoApiTes")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee435b40-3a69-4457-9b6f-7b01ea136089")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DemoApiTes/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApiDemo.Controllers;
+
+namespace DemoApiTes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public static void TestMethod1()
+        {
+            ValuesController valuesController = new ValuesController();
+            
+
+        }
+    }
+}
